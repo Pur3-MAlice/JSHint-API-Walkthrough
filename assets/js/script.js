ce95c94400fc46eb25c45427b03891123fd2828c
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-17 15:21:07 +0000

    script.js api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 const API_KEY = "8HLZXrA3BquTJ4oKfIx-bPGfuoc";
-const API_URL ="https://ci-jshint.herokuapp.com/api";
-const resultsModal = new bootstrap.Modal(document.getElementsById("resultsModal"));
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) {
 
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
-// Notes on the API. Saved for later.
-// https://ci-jshint.herokuapp.com/api?api_key=thisismykey
-// https://ci-jshint.herokuapp.com/api?api_key=8HLZXrA3BquTJ4oKfIx-bPGfuoc 
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
