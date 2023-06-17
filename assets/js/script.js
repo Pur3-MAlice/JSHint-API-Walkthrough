156adbb28b9b4decf8131c3de5db0e64afbbabff
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-17 14:58:51 +0000

    adds variables for the api to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const API_KEY = "8HLZXrA3BquTJ4oKfIx-bPGfuoc";
+const API_URL ="https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementsById("resultsModal"));
+
+
+
+
+// Notes on the API. Saved for later.
+// https://ci-jshint.herokuapp.com/api?api_key=thisismykey
+// https://ci-jshint.herokuapp.com/api?api_key=8HLZXrA3BquTJ4oKfIx-bPGfuoc 
