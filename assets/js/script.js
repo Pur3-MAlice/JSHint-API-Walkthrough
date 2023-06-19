c68e4b7709b852d2244b658deb34d276e5649d56
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-19 14:01:21 +0000

    adds process options function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,27 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
+
 async function postForm(e) {
 
     const form = new FormData(document.getElementById("checksform"));
-    
+
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
@@ -27,10 +44,6 @@ async function postForm(e) {
 
 }
 
-function displayErrors(data) {
-
-}
-
 async function getStatus(e) {
 
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -44,8 +57,29 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
+
 }
 
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
 
 function displayStatus(data) {
 
@@ -56,5 +90,5 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
-}
 
+}
